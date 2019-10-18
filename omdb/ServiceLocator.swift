8b7ef51aa9fc4bd0eb9d2a51389b060ef6cb9a45
Luis Martínez Zarza <lfmartinez@atsistemas.com>
2019-10-18 18:49:01 +0200

    Partial folder structure. Wireframe and ServiceLocator singletons.

---
+++ omdb/ServiceLocator.swift
@@ -0,0 +1,15 @@
+//
+//  ServiceLocator.swift
+//  omdb
+//
+//  Created by Luis Martínez Zarza on 18/10/2019.
+//  Copyright © 2019 Luis Martínez Zarza. All rights reserved.
+//
+
+import UIKit
+
+class ServiceLocator {
+    static let shared = ServiceLocator()
+    
+    private init() {}
+}
