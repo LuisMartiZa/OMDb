9d7c58eab37209a5f4464f61226367374ad1907c
Luis Martínez Zarza <lfmartinez@atsistemas.com>
2019-10-18 18:34:05 +0200

    Initial Commit

---
+++ omdb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  omdb
+//
+//  Created by Luis Martínez Zarza on 18/10/2019.
+//  Copyright © 2019 Luis Martínez Zarza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
