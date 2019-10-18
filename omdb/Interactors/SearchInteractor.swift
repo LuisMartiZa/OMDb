18371cd7f9905f8f6f12464cc352ac73ddf77465
Luis Martínez Zarza <lfmartinez@atsistemas.com>
2019-10-18 21:39:44 +0200

    Add design for table view cell. Add rest of classes dummies.

---
+++ omdb/Interactors/SearchInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  SearchInteractor.swift
+//  omdb
+//
+//  Created by Luis Martínez Zarza on 18/10/2019.
+//  Copyright © 2019 Luis Martínez Zarza. All rights reserved.
+//
+
+import Foundation
--- omdb/Screens/SearchView/Cells/SearchTableViewCell.swift
+++ omdb/Screens/SearchView/Cells/SearchTableViewCell.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2019 Luis Martínez Zarza. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Kingfisher
+
+class SearchTableViewCell: UITableViewCell {
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    func setData(from searchItem: SearchItem) {
+        self.titleLabel.text = searchItem.title
+        self.yearLabel.text = searchItem.year
+        let urlImage = URL(string: searchItem.posterImage!)
+        self.posterImageView.kf.setImage(with: urlImage)
+    }
+}
